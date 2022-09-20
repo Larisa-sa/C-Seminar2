6082f10e38be68f56b7f70a4aa6ccc9810a0c0db
Larisa <larisamash01@gmail.com>
2022-09-20 11:34:55 +0300

    Решение 3 задачи из домашки

---
+++ homework3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введи цифру, обозначающую день недели: ");
+int dayNumber = int.Parse(Console.ReadLine());
+if (dayNumber == 6 || dayNumber == 7)
+{
+  Console.WriteLine(dayNumber + " Это выходной? " + " -> " + "да");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+  Console.WriteLine("Дня недели с такой цифрой не существует");
+}
+else
+{
+Console.WriteLine(dayNumber + " Это выходной? " + " -> " + "нет");
+}
+
+
+
+
